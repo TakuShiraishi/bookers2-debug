8a8fcca95985ba90f487d6ac50a3204b350e7b1b
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-10-04 13:18:23 +0000

    応用課題７a

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,9 @@ class Book < ApplicationRecord
   validates :body,presence:true,length:{maximum:200}
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
- has_many :favorited_users, through: :favorites, source: :user
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).
+  at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
+
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
