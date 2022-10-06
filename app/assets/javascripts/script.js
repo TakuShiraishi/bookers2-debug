a109d4173b43a129d647ff4c44d19ce2485c7fb1
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-10-06 06:42:29 +0000

    応用課題8b

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,8 @@
+
+$(document).on('turbolinks:load', function() { });
+
+
+$(document).on('turbolinks:render', function() {　});
+
+
+$(document).on('turbolinks:request-start', function() {　});avascripts/script.js
