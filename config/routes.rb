ff264632e8762903152f46f5091bbe42fb3add1d
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-10-06 09:17:53 +0000

    応用課題9b

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about" => "homes#about", as: "about"
 
-  resources :books do
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]  do
    resources :book_comments, only: [:create, :destroy]
    resource  :favorites, only: [:create, :destroy]
   end
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
+  	get "daily_posts" => "users#daily_posts"
   end
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    get '/search', to: 'searchs#search'
